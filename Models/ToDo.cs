2cae2284a97a621b2babfcab9a35ad5b39ea955c
Mozart Al Khateeb <mozart.alkhateeb@gmail.com>
2019-06-16 00:23:17 +0300

    Initial project with read Todo list and some styling

---
+++ Models/ToDo.cs
@@ -0,0 +1,9 @@
+namespace ToDo.Blazor.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
